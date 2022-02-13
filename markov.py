07df3331e5d7c3d4422e36d9b72fe882d3d6e57d
Chengfeng Ren <chengfengren@gmail.com>
2022-02-13 01:34:47 +0000

    updated functions and features

--- markov.py
+++ markov.py
@@ -1,5 +1,9 @@
 """Generate Markov text from text files."""
 
+import sys
+# print()
+# print(sys.argv)
+
 from random import choice
 
 def open_and_read_file(file_path):
@@ -12,12 +16,16 @@ def open_and_read_file(file_path):
     contents = file.read()
     file.close()  
 
+    # file = with open(file_path).read()
+
     return contents #'Contents of your file as one long string'
 # print(open_and_read_file("green-eggs.txt"))
 # contents = open_and_read_file(file_path)
+# break()  #stops till here. won't run the following. 
+
 def make_chains(contents):
     """Take input text as string; return dictionary of Markov chains.
-
+ 
     A chain will be a key that consists of a tuple of (word1, word2)
     and the value would be a list of the word(s) that follow those two
     words in the input text.
@@ -43,57 +51,115 @@ def make_chains(contents):
     chains = {}
     words = contents.split() #words is a list of words
 
-    for i in range(len(words) - 1):
-        key = (words[i], words[i + 1])
-        if words[i+1] == words[-1]:
-            break
-        else:
-            value = (words[i + 2])        
-        if key not in chains.keys():
-            chains[key] = []
-            chains[key].append(value)
-        else:
-            chains[key].append(value)
+    # for i in range(len(words) - 1):
+    #     key = (words[i], words[i + 1])
+    #     if words[i+1] == words[-1]:
+    #         break
+    #     else:
+    #         value = (words[i + 2])        
+    #     if key not in chains.keys():
+    #         chains[key] = [value]
+    #         # chains[key].append(value)
+    #     else:
+    #         chains[key].append(value)
         
+    i = 0
+    for i in range(len(words) - 2):
+        key = (words[i], words[i+1]) #creating tuple
 
-         # list = [1,2,3,4]
+        if key in chains:
+            #append value of words at i+2, append to the dictionary at teh value of key
+            chains[key].append(words[i+2])
+        else:
+            #making a new list as the value
+            value = [words[i+2]]
+            #make th evalue of words at i+2
+            chains[key] = value
 
     return chains
 
 # chains=make_chains(contents)
 
 
+# def make_text(chains): 
+#     """Return text from chains."""
+#     words= []
+
+#     key = choice(list(chains.keys())) # key is a tuple
+#     chosen_word = choice(chains[key]) # chosen_word is a string
+
+#     words.extend(list(key))
+#     words.append(chosen_word)
+
+#     new_key = (key[1], chosen_word)
+
+#     while new_key in chains.keys():
+#         chosen_word = choice(chains[new_key])
+#         words.append(chosen_word)
+#         new_key = (new_key[1], chosen_word)
+
+#     return ' '.join(words)
+
 def make_text(chains):
     """Return text from chains."""
-    words= []
 
-    key = choice(list(chains.keys())) # key is a tuple
-    chosen_word = choice(chains[key]) # chosen_word is a string
-    # link = key+chosen_word
-    words.append(key[0])
-    words.append(key[1])
-    words.append(chosen_word)
+    words = []
+    #pick a key (word1, word2)
+    keys = chains.keys() # returns list of tuples as dict_keys datatype, which is not subscriptable
+    keys_list = list(keys) #transtype to proper subscriptable list
+    word_pair = choice(keys_list) #randomly pick a tuple from list
+    words.append(word_pair[0]) #append first word of tuple to words list
+    
+    while True:
+        # if word_pair == ('Sam', 'I'):
+        #     words.append(('Sam', 'I')[1])
+        #     words.append('I')
+            
+        words.append(word_pair[1])
+        
+        if word_pair in chains:
+            #pick a random word from the list of words associated with key word pair (word3)    
+            next_word = choice(chains[word_pair])
+            word_pair = (word_pair[1], next_word) #create next word-pair from second word of previous key-pair and the next word
+            #last word of first key-pair and the word we just picked for 'next-word' and make the next tuple (word2, word3)
+    
+        else:
+            break
 
-    new_key = (key[1], chosen_word)
+    # While True: repeat, and build words list. Once a word-pair is created that is not present in original chains
+    # true loop will break, and we will join all words in words list to form a string sentence
 
-    while new_key in chains.keys():
-        chosen_word = choice(chains[new_key])
-        words.append(chosen_word)
-        new_key = (new_key[1], chosen_word)
-
-    return ' '.join(words)
+    return ' '.join(words) #create string from words list
 
 # print(make_text(chains))
 
-file_path = 'green-eggs.txt'
+#############################
+# using sys.argv to process a file. Pass in this file from the command line, using sys.argv.
+# file_path = sys.argv[1] # a string
+    # contents = open_and_read_file(file_path)
 
-# Open the file and turn it into one long string
-contents = open_and_read_file(file_path)
+    # # Get a Markov chain
+    # chains = make_chains(contents)
 
-# Get a Markov chain
-chains = make_chains(contents)
+    # # Produce random text
+    # random_text = make_text(chains)
 
-# Produce random text
-random_text = make_text(chains)
+    # print(random_text)
+#####################################
 
-print(random_text)
+# using sys.argv to process multiple files, one by one. Pass in files from the command line, using sys.argv.
+for file_path in sys.argv[1:]:
+
+    # Open the file and turn it into one long string
+    contents = open_and_read_file(file_path)
+
+    # Get a Markov chain
+    chains = make_chains(contents)
+
+    # Produce random text
+    random_text = make_text(chains)
+
+    print(random_text)
+
+
+
